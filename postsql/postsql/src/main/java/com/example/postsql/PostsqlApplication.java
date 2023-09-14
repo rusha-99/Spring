19a4569a2a12089e0e5a629af91c846977d733ea
Tharusha Pathirana <91452474+rusha-99@users.noreply.github.com>
2023-09-14 20:03:55 +0530

    project two initialize

---
+++ postsql/postsql/src/main/java/com/example/postsql/PostsqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.postsql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostsqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostsqlApplication.class, args);
+	}
+
+}
